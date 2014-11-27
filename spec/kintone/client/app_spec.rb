d269fb813dd2d7d45fdf663675a3ab62775056a0
Genki Sugawara <sugawara@cookpad.com>
2014-11-27 15:11:21 +0900

    Rename spec

--- spec/kintone/client/app_spec.rb
+++ spec/kintone/client/app_spec.rb
@@ -1,3 +1,4 @@
+# https://cybozudev.zendesk.com/hc/ja/articles/202931674-%E3%82%A2%E3%83%97%E3%83%AA%E6%83%85%E5%A0%B1%E3%81%AE%E5%8F%96%E5%BE%97#step1
 describe Kintone::Client do
   describe 'アプリ情報の取得(1件)' do
     let(:response) do
