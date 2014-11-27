04c7a7a86fa27b05d9d9060fc582d88d62fb4102
Genki Sugawara <sugawara@cookpad.com>
2014-11-27 15:27:34 +0900

    Fix spec

--- spec/kintone/client/app_spec.rb
+++ spec/kintone/client/app_spec.rb
@@ -1,6 +1,10 @@
 # https://cybozudev.zendesk.com/hc/ja/articles/202931674-%E3%82%A2%E3%83%97%E3%83%AA%E6%83%85%E5%A0%B1%E3%81%AE%E5%8F%96%E5%BE%97#step1
 describe Kintone::Client do
   describe 'アプリ情報の取得(1件)' do
+    let(:request) do
+      {"id"=>"4"}
+    end
+
     let(:response) do
       {"appId"=>"1",
        "code"=>"",
@@ -17,7 +21,7 @@ describe Kintone::Client do
     subject do
       kintone_client {|stub|
         stub.get('/k/v1/app.json') do |env|
-          expect(env.params).to eq({"id"=>"4"})
+          expect(env.params).to eq request
           expect(env.request_headers['X-Cybozu-Authorization']).to eq TEST_AUTH_HEADER
           [200, {'Content-Type' => 'json'}, JSON.dump(response)]
         end
